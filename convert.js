4d1d2e7b7b5c62379c95815815cda8935745db49
jhraju <jhraju890@gmail.com>
2024-02-04 01:03:50 +0600

    added some converts

---
+++ convert.js
@@ -0,0 +1,16 @@
+// 12 inch 1 feet//
+function inchToFeet(inch){
+const feet = inch/ 12;
+return feet
+}
+
+const rajuHeight = inchToFeet(70);
+console.log(rajuHeight); 
+//---------------------------------------------
+function milesToKilometer(miles){
+const kilo = miles * 1.60;
+return kilo;
+}
+
+const kilo = milesToKilometer(10);
+console.log(kilo);
